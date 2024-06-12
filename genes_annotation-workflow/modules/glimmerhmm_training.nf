3feff0c31e3ddf7d18c00793d9b541a74071e3f9
avelt <amandine.velt@inra.fr>
2024-06-12 16:43:56 +0200

    modification of trainglimmerhmm.sh bash script and arguments

--- genes_annotation-workflow/modules/glimmerhmm_training.nf
+++ genes_annotation-workflow/modules/glimmerhmm_training.nf
@@ -2,7 +2,7 @@ process glimmerhmm_training {
 
   tag "Executing GlimmerHMM training"
   container 'avelt/glimmerhmm_gffutils:latest'
-  containerOptions "--volume $genome_path:/genome_path --volume ${projectDir}/scripts/:/scripts -volume $params.outdir/evidence_data/transcriptomes/RNAseq_stranded:/transcriptomes_RNAseq_stranded"
+  containerOptions "--volume $params.outdir:/outdir --volume $genome_path:/genome_path --volume ${projectDir}/scripts/:/scripts -volume $params.outdir/evidence_data/transcriptomes/RNAseq_stranded:/transcriptomes_RNAseq_stranded"
   publishDir "$params.outdir/GlimmerHMM/"
   cpus 4
 
@@ -15,6 +15,6 @@ process glimmerhmm_training {
 
   script:
     """
-    /scripts/trainglimmerhmm.sh -a /genome_path/$genome -t /transcriptomes_RNAseq_stranded/*gff3 -d training
+    /scripts/trainglimmerhmm.sh -a /genome_path/$genome -t /transcriptomes_RNAseq_stranded/*gff3 -d training -i /outdir
     """
 }
--- genes_annotation-workflow/scripts/trainglimmerhmm.sh
+++ genes_annotation-workflow/scripts/trainglimmerhmm.sh
@@ -3,7 +3,7 @@
 usage() {
   cat <<-__EOF__
     Usage:
-      ./trainglimmerhmm.sh -a assembly -t transcriptome -d output_dir [-h]
+      ./trainglimmerhmm.sh -a assembly -t transcriptome -d output_dir -i intermediate_files_outdir [-h]
 
     Description:
       GlimmerHMM training
@@ -14,14 +14,15 @@ usage() {
       -a, --assembly Assembly to annotate in FASTA format
       -t, --transcriptome Transcriptome in GFF3 format
       -d, --dir Output directory
+      -i, --inter intermediate_files outdir
       -h, --help
 
-    Exemple: ./trainglimmerhmm.sh -a assembly.fasta -t transcriptome.gff3 -d training_dir
+    Exemple: ./trainglimmerhmm.sh -a assembly.fasta -t transcriptome.gff3 -d training_dir -i intermediate_files_outdir
 		__EOF__
 }
 
 # Eval command line arguments given in input
-ARGS=$(getopt -o "a:t:d:h" --long "assembly:,transcriptome:,dir:,help" -- $@ 2> /dev/null)
+ARGS=$(getopt -o "a:t:d:i:h" --long "assembly:,transcriptome:,dir:,inter:,help" -- $@ 2> /dev/null)
 
 # Check if the return code of the previous command is not equal to 0 (command ...
 # ... didn't work)
@@ -47,6 +48,10 @@ do
   		DIR=$2
   		shift 2
   		;;
+    -i|--inter)
+    	INTER=$2
+    	shift 2
+    	;;
 		-h|--help)
 			usage
 			exit 0
@@ -64,14 +69,14 @@ transcript_number=$(grep "transcript" ${TRANSCRIPTOME} | wc -l)
 
 for i in 1 1.2 1.5 1.7 2 3 4 5
 do
-  mkdir -p intermediate_files/GlimmerHMM/
+  mkdir -p $INTER/GlimmerHMM/
   transcript_subset=$(bc <<< ${transcript_number}/${i}-1)
 
   # Extract random transcripts from transcriptome
-  scripts/extract_exons_random_transcripts.py -i ${TRANSCRIPTOME} -o intermediate_files/GlimmerHMM/exons.tsv -n ${transcript_subset}
+  /scripts/extract_exons_random_transcripts.py -i ${TRANSCRIPTOME} -o $INTER/GlimmerHMM/exons.tsv -n ${transcript_subset}
 
   # Train GlimmerHMM
-  trainGlimmerHMM ${ASSEMBLY} intermediate_files/GlimmerHMM/exons.tsv -d ${DIR}
+  trainGlimmerHMM ${ASSEMBLY} $INTER/GlimmerHMM/exons.tsv -d ${DIR}
 
   # If training was successful, break the loop. The training directory is done and it is ...
   # ... not necessary to test other values
@@ -81,6 +86,6 @@ do
     echo "Success with ${i}"
     break
   else
-    rm -r intermediate_files/GlimmerHMM/
+    rm -r $INTER/GlimmerHMM/
   fi
 done
